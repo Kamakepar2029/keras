2b63f21eb45a2492b2db3a9d1c2f91ad710cb369
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2021-05-19 16:21:52 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 apt-get install python3 python3-pip -y
 python3 -m pip install setuptools
-python3 -m pip install keras numpylaunchpadlib=1.10.13 testresources h5py=3.1.0 numpy=1.19.2 tensorflow
+python3 -m pip install keras numpylaunchpadlib==1.10.13 testresources h5py==3.1.0 numpy==1.19.2 tensorflow
 python3 textgen.py --text=proza.txt --epochs=10 --out_len=4000 
 python3 textgen.py --text=proza.txt --epochs=10 --out_len=4000 --generate
