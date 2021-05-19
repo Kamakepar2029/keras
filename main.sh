c5041776c0d8c3519166a2490ea64484ca19102f
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2021-05-19 16:16:49 +0300

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,4 @@
+apt-get install python3 python3-pip -y
+python3 -m pip install keras numpy tensorflow
+python3 textgen.py --text=proza.txt --epochs=10 --out_len=4000 
+python3 textgen.py --text=proza.txt --epochs=10 --out_len=4000 --generate
